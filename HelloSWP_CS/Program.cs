91c9474806da1db131c683fdb7b47f7a29ac8927
berkinn61 <119922590+berkinn61@users.noreply.github.com>
2024-09-20 11:45:02 +0200

    Assignment completed

--- HelloSWP_CS/Program.cs
+++ HelloSWP_CS/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, SWP !");
+﻿using System;
+ 
+class Program
+{
+    static void Main()
+    {
+        string input;
+ 
+        while (true)
+        {
+            Console.WriteLine("Bitte eine Eingabe tätigen (oder 'Beenden' zum Beenden):");
+ 
+            input = Console.ReadLine();
+ 
+            if (input.Equals("Beenden", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Programm wird beendet.");
+                break;
+            }
+ 
+            Console.WriteLine("Eingegebener Text: " + input);
+        }
+    }
+}
+ 
