659813bd6175d6e9bad8aa057371d232e139278c
berkinn61 <119922590+berkinn61@users.noreply.github.com>
2024-09-27 11:35:24 +0200

    Assignment 2_done

---
+++ assignmentTwo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class ZahlEingabe
+{
+    static void Main()
+    {
+        string benutzereingabe; 
+        int eingegebeneZahl;   
+
+        while (true) 
+        {
+            Console.WriteLine("Bitte geben Sie eine natürliche Zahl ein (oder 'stop' zum Beenden):");
+            benutzereingabe = Console.ReadLine();
+
+            
+            if (benutzereingabe.Equals("stop", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Das Programm wird jetzt beendet.");
+                break;
+            }
+
+           
+            if (int.TryParse(benutzereingabe, out eingegebeneZahl) && eingegebeneZahl >= 0)
+            {
+                Console.WriteLine("Sie haben die Zahl eingegeben: " + eingegebeneZahl);
+            }
+            else
+            {
+                Console.WriteLine("Ungültige Eingabe! Bitte geben Sie eine gültige natürliche Zahl ein.");
+            }
+        }
+    }
+}
