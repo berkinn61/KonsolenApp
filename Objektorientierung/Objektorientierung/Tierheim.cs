75695a947ee150d7692ed6153a9ce91ada0a0339
berkinn61 <119922590+berkinn61@users.noreply.github.com>
2024-12-06 12:07:05 +0100

    Anzahl von Katzen und Hunde

---
+++ Objektorientierung/Objektorientierung/Tierheim.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objektorientierung
+{
+    class Tierheim
+    {
+        public List<Animal> tiere = new List<Animal>();
+        public void AddAnimal(Animal animal)
+        {
+            tiere.Add(animal);
+            Console.WriteLine($"\nEs wurde neues {animal.TierTyp} {animal} hinzugefügt!\n");
+        }
+        public void ShowAnimals()
+        {
+            int index = 1;
+            foreach (var cat in tiere)
+            {
+                Console.WriteLine(cat);
+            }
+        }
+        public int NumberOfCats
+        {
+            get { return tiere.Select(a => a.GetType() == typeof(Cat)).Count(); }
+        }
+
+        public int NumberOfDogs
+        {
+            get { return tiere.Select(a => a.GetType() == typeof(Dog)).Count(); }
+        }
+    }
+}
